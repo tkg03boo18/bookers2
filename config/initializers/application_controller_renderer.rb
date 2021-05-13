adac64753cd8b940cc946aa83ce61f53dff87af6
tkg03boo18 <tkg03boo18@gmail.com>
2021-05-13 13:50:01 +0000

    [Update]4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '00c33538caf0abe49728bdfef76de01b4181c8144cadff8e9105108e65aa32f6d80720472f70cd59d8e272c823c795505f65ef67301b1437fb618068e9fdad0e'
