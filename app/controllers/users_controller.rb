f137b43d44c79af681d026ad08091a0dca5b944b
tkg03boo18 <tkg03boo18@gmail.com>
2021-05-13 06:54:58 +0000

    [Update]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = Book.new
-    @books = @user.books.page(params[:page])
+    @books = current_user.books
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+
   root to: 'homes#top'
+
+  devise_for :users
+
   get 'homes/about' => 'homes#about', as:'about'
   resources :books, only: [:new, :create, :index, :show, :destroy]
 
