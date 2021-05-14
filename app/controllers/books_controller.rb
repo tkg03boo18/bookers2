178a0e68abbb6d84e494cd8c3e4c60af60be1f7a
tkg03boo18 <tkg03boo18@gmail.com>
2021-05-14 08:12:27 +0000

    [Update]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,18 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    @book.update
+    redirect_to book_path(@book.id)
   end
 
   def index
@@ -17,6 +28,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = current_user
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'homes/about' => 'homes#about', as:'about'
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :edit, :update, :index, :show, :destroy]
 
   resources :users, only: [:show, :index, :edit, :update]
 end
