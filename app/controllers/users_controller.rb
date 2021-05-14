449fc4742b5fc38666b3a964942f85cf6e601dbc
tkg03boo18 <tkg03boo18@gmail.com>
2021-05-14 12:12:16 +0000

    [Update]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @book = Book.new
-    @books = current_user.books
+    # @books = current_user.books
+    @books = @user.books.page(params[:page]).reverse_order
   end
 
   def edit
