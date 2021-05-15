68c3427febfab9e403e7a813fb41a63945217a0b
tkg03boo18 <tkg03boo18@gmail.com>
2021-05-15 15:11:40 +0000

    [Update]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.new
+    @new_book = Book.new
     @book = Book.find(params[:id])
     @user = current_user
   end
